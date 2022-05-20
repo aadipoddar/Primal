1f6bee4d2307bbd62d48bea25549e4d93e82823b
Aadi Poddar <aadipoddarmail@gmail.com>
2022-05-21 03:11:45 +0530

    Loading project templates for game projects

---
+++ PrimalEditor/Utilities/Serializer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.Serialization;
+
+namespace PrimalEditor.Utilities
+{
+    public static class Serializer
+    {
+        /* used to write to file */
+        public static void ToFile<T>(T Instance, string Path)
+        {
+            try
+            {
+                using var fs = new FileStream(Path, FileMode.Create);
+                var Serializer = new DataContractSerializer(typeof(T));
+                Serializer.WriteObject(fs, Instance);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                // TODOL: update log
+            }
+        }
+
+        internal static T FromFile<T>(string Path)
+        {
+            try
+            {
+                using var fs = new FileStream(Path, FileMode.Open);
+                var Serializer = new DataContractSerializer(typeof(T));
+                T instance = (T)Serializer.ReadObject(fs);
+                return instance;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                // TODOL: update log
+                return default(T);
+            }
+        }
+    }
+}
