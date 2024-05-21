f33334f18127bf488d18adb1396b7a45d3483005
Aadi Poddar <aadipoddarmail@gmail.com>
2024-05-21 11:48:49 +0530

    Project Setup and Basic Project Dialog

---
+++ PrimalEditor/GameProject/ProjectBrowserDialog.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+
+namespace PrimalEditor.GameProject
+{
+	/// <summary>
+	/// Interaction logic for ProjectBrowserDialog.xaml
+	/// </summary>
+	public partial class ProjectBrowserDialog : Window
+	{
+		public ProjectBrowserDialog()
+		{
+			InitializeComponent();
+		}
+
+		private void OnToggleButton_Click(object sender, RoutedEventArgs e)
+		{
+			if (sender == openProjectButton)
+			{
+				if (createProjectButton.IsChecked == true)
+				{
+					createProjectButton.IsChecked = false;
+					browserContent.Margin = new Thickness(0);
+				}
+				openProjectButton.IsChecked = true;
+			}
+
+			else
+			{
+				if (openProjectButton.IsChecked == true)
+				{
+					openProjectButton.IsChecked = false;
+					browserContent.Margin = new Thickness(-800, 0, 0, 0);
+				}
+				createProjectButton.IsChecked = true;
+			}
+		}
+	}
+}
---
+++ PrimalEditor/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+
+using PrimalEditor.GameProject;
+
+namespace PrimalEditor
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			Loaded += OnMainWindowLoaded;
+		}
+
+		private void OnMainWindowLoaded(object sender, RoutedEventArgs e)
+		{
+			Loaded -= OnMainWindowLoaded;
+			OpenProjectBrowserDialog();
+		}
+
+		private void OpenProjectBrowserDialog()
+		{
+			var projectBrowser = new ProjectBrowserDialog();
+			if (projectBrowser.ShowDialog() == false)
+			{
+				Application.Current.Shutdown();
+			}
+			else
+			{
+
+			}
+		}
+	}
+}
