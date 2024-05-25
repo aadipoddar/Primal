1a8c2304e3e73ff6cecada3002556f10f84cd603
Aadi Poddar <aadipoddarmail@gmail.com>
2024-05-25 23:58:11 +0530

    UI restyling and animations for project browser
    - Implemented Dark UI theme and animations

--- PrimalEditor/Dictionaries/ControlTemplates.xaml.cs
+++ PrimalEditor/Dictionaries/ControlTemplates.xaml.cs
@@ -31,5 +31,24 @@ namespace PrimalEditor.Dictionaries
 				Keyboard.ClearFocus();
 			}
 		}
+
+		private void OnClose_Button_Click(object sender, RoutedEventArgs e)
+		{
+			var window = (Window)((FrameworkElement)sender).TemplatedParent;
+			window.Close();
+		}
+
+		private void OnMaximizeRestore_Button_Click(object sender, RoutedEventArgs e)
+		{
+			var window = (Window)((FrameworkElement)sender).TemplatedParent;
+			window.WindowState = (window.WindowState == WindowState.Normal) ?
+				WindowState.Maximized : WindowState.Normal;
+		}
+
+		private void OnMinimize_Button_Click(object sender, RoutedEventArgs e)
+		{
+			var window = (Window)((FrameworkElement)sender).TemplatedParent;
+			window.WindowState = WindowState.Minimized;
+		}
 	}
 }
--- PrimalEditor/GameProject/ProjectBrowserDialog.xaml.cs
+++ PrimalEditor/GameProject/ProjectBrowserDialog.xaml.cs
@@ -1,12 +1,15 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
 
 namespace PrimalEditor.GameProject
 {
 	/// <summary>
-	/// Interaction logic for ProjectBrowserDialog.xaml
+	/// Interaction logic for ProjectBrowserDialg.xaml
 	/// </summary>
 	public partial class ProjectBrowserDialog : Window
 	{
+		private readonly CubicEase _easing = new CubicEase() { EasingMode = EasingMode.EaseInOut };
 		public ProjectBrowserDialog()
 		{
 			InitializeComponent();
@@ -24,6 +27,32 @@ namespace PrimalEditor.GameProject
 			}
 		}
 
+		private void AnimateToCreateProject()
+		{
+			var highlightAnimation = new DoubleAnimation(200, 400, new Duration(TimeSpan.FromSeconds(0.2)));
+			highlightAnimation.EasingFunction = _easing;
+			highlightAnimation.Completed += (s, e) =>
+			{
+				var animation = new ThicknessAnimation(new Thickness(0), new Thickness(-1600, 0, 0, 0), new Duration(TimeSpan.FromSeconds(0.5)));
+				animation.EasingFunction = _easing;
+				browserContent.BeginAnimation(MarginProperty, animation);
+			};
+			highlightRect.BeginAnimation(Canvas.LeftProperty, highlightAnimation);
+		}
+
+		private void AnimateToOpenProject()
+		{
+			var highlightAnimation = new DoubleAnimation(400, 200, new Duration(TimeSpan.FromSeconds(0.2)));
+			highlightAnimation.EasingFunction = _easing;
+			highlightAnimation.Completed += (s, e) =>
+			{
+				var animation = new ThicknessAnimation(new Thickness(-1600, 0, 0, 0), new Thickness(0), new Duration(TimeSpan.FromSeconds(0.5)));
+				animation.EasingFunction = _easing;
+				browserContent.BeginAnimation(MarginProperty, animation);
+			};
+			highlightRect.BeginAnimation(Canvas.LeftProperty, highlightAnimation);
+		}
+
 		private void OnToggleButton_Click(object sender, RoutedEventArgs e)
 		{
 			if (sender == openProjectButton)
@@ -31,17 +60,20 @@ namespace PrimalEditor.GameProject
 				if (createProjectButton.IsChecked == true)
 				{
 					createProjectButton.IsChecked = false;
-					browserContent.Margin = new Thickness(0);
+					AnimateToOpenProject();
+					openProjectView.IsEnabled = true;
+					newProjectView.IsEnabled = false;
 				}
 				openProjectButton.IsChecked = true;
 			}
-
 			else
 			{
 				if (openProjectButton.IsChecked == true)
 				{
 					openProjectButton.IsChecked = false;
-					browserContent.Margin = new Thickness(-800, 0, 0, 0);
+					AnimateToCreateProject();
+					openProjectView.IsEnabled = false;
+					newProjectView.IsEnabled = true;
 				}
 				createProjectButton.IsChecked = true;
 			}
