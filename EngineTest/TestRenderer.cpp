c958977ade6bde58c908bfe9c31321929154cfc0
Aadi Poddar <aadipoddarmail@gmail.com>
2024-06-19 23:07:28 +0530

    Fix
    - Changed "ifdef" to "if" because with ifde this code is always included whereas "if" includes this code when test renderer has a non zero value
    - Changed the name of test windows

--- EngineTest/TestRenderer.cpp
+++ EngineTest/TestRenderer.cpp
@@ -2,7 +2,7 @@
 #include "..\Platform\Platform.h"
 #include "..\Graphics\Renderer.h"
 #include "TestRenderer.h"
-#ifdef TEST_RENDERER
+#if TEST_RENDERER
 
 using namespace primal;
 
@@ -63,10 +63,10 @@ engine_test::initialize()
 
 	platform::window_init_info info[]
 	{
-		{&win_proc, nullptr, L"Test window 1", 100, 100, 400, 800},
-		{&win_proc, nullptr, L"Test window 2", 150, 150, 800, 400},
-		{&win_proc, nullptr, L"Test window 3", 200, 200, 400, 400},
-		{&win_proc, nullptr, L"Test window 4", 250, 250, 800, 600},
+		{&win_proc, nullptr, L"Render window 1", 100, 100, 400, 800},
+		{&win_proc, nullptr, L"Render window 2", 150, 150, 800, 400},
+		{&win_proc, nullptr, L"Render window 3", 200, 200, 400, 400},
+		{&win_proc, nullptr, L"Render window 4", 250, 250, 800, 600},
 	};
 	static_assert(_countof(info) == _countof(_surfaces));
 
